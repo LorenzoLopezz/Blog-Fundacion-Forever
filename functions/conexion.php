f159cdc92fd8d03696d556efe03881bc641a9e08
LorenzoLopezz <lorenzolopezdepaz@gmail.com>
2022-08-24 20:01:08 -0600

    Dejando bk de base de datos utilizable para pruebas
    Proyecto archivado, nunca puesto en producciòn

--- functions/conexion.php
+++ functions/conexion.php
@@ -1,6 +1,5 @@
 <?php 
 
-	// $con = new mysqli("localhost","root","","periodico");
-	$con = new mysqli("localhost","id6267900_lorenzo","fsociety1998","id6267900_periodico");
+	$con = new mysqli("localhost","root","","periodico");
 
 ?>
